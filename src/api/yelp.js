4d58d6b8039c192f95cf501e8e95baad01f0cabb
Neba Emmanuel <nebaemmanuel736@gmail.com>
2023-03-18 15:32:26 +0100

    rendered fetched data

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-expot default axios.create({
+export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
         Authorization: 'Bearer p2oEWN9KHLbfCgGsMhVzvEzKe5qZu368mit6-hJ13VhjB_pZHsLKID5g5l6DDBc_fmvaui8_KtaxDWa2fcO2Z9SrwbBgW-L24JgAhm3JVfRCkaj2h5N6PMZ2F2sVZHYx'
